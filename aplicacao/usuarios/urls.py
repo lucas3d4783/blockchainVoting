d7e7fe168c58b9acc876ece729d0734239a3b71e
Lucas Reichert <lucas3d4783@gmail.com>
2020-01-14 08:15:14 -0300

    organização da navegação da página

--- aplicacao/usuarios/urls.py
+++ aplicacao/usuarios/urls.py
@@ -4,7 +4,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', views.consulta, name='consulta'),
-    url('/cadastro/', views.cadastro, name='cadastro'),
+    path('', views.index, name='index_usuario'),
+    path('consulta/', views.consulta, name='consulta'),
+    url('cadastro/', views.cadastro, name='cadastro'),
     url(r'^edit/(?P<pk>[0-9]+)$', views.edicao, name='edicao'),
 ]
--- aplicacao/usuarios/views.py
+++ aplicacao/usuarios/views.py
@@ -7,6 +7,9 @@ from django.utils import timezone
 import hashlib
 from django.shortcuts import redirect
 
+def index(request): #quando for solicitado o url index, será encaminhado o index.html
+    return render(request, 'usuarios/index.html')
+
 def cadastro(request): #  Criação de usuários 
     form = CadastroForm(request.POST or None) # o formulário pode estar vázio ou não, se estiver apenas carregando o formulário
     context = {'form': form} # variável utilizada para encaminhar as informações para a tela de cadastro
