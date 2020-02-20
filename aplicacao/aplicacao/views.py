2402cdda3cb75505224f15718fd5d64158d50b1f
lucas3d4783 <lucas3d4783@gmail.com>
2020-02-20 18:48:36 -0300

    inicialização da tela de login

--- aplicacao/aplicacao/views.py
+++ aplicacao/aplicacao/views.py
@@ -1,5 +1,7 @@
 # _*_ coding: utf-8 _*_ 
 from django.shortcuts import render
+from usuarios.models import Usuario
+from django.http import HttpResponse
 
 
 def index(request): #quando for solicitado o url index, será encaminhado o index.html
@@ -18,4 +20,23 @@ def sobre(request): #quando for solicitado o url sobre, será encaminhado o inde
         'title': title,
     }
     return render(request, 'sobre.html', context)
+
+
+#https://django-portuguese.readthedocs.io/en/1.0/topics/http/sessions.html
+#https://docs.djangoproject.com/en/3.0/topics/http/sessions/
+
+def login(request):
+    u = Usuario.objects.get(usuario=request.POST['usuario'])
+    if u.senha == request.POST['senha']:
+        request.session['member_id'] = u.pk
+        return HttpResponse(u"Você está autenticado.")
+    else:
+        return HttpResponse(u"Seu nome de usuário e senha não conferem.")
+
+def logout(request):
+    try:
+        del request.session['member_id']
+    except KeyError:
+        pass
+    return HttpResponse(u"Você saiu.")
 
